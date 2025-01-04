91f38e79be967f04a4429e1d7be6b325b397ac50
Vinayak Thakur <142525931+Cobrachainsaw@users.noreply.github.com>
2025-01-04 13:43:33 +0530

    Add files via upload

---
+++ api.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr  string
+	store Store //repository having the connections to the database
+}
+
+func NewAPIServer(addr string, store Store) *APIServer {
+	return &APIServer{addr: addr, store: store}
+}
+
+func (s *APIServer) Serve() { //initialize the router then register services
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	//register service
+	tasksService := newTasksService((s.store))
+	tasksService.RegisterRoutes(router)
+
+	log.Println("Starting the API Server at", s.addr)
+
+	log.Fatal(http.ListenAndServe(s.addr, subrouter))
+}
